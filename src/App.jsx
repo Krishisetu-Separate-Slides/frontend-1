d9ea97cd52659574f4c9935c7c5adc5b0e2bcb5f
chhaya03 <0188chhayamishra@gmail.com>
2024-10-31 12:00:13 +0530

    comment

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import Slide4 from "./components/Slide4";
 import Slide5 from "./components/Slide5";
 import Slide7 from "./components/Slide7";
 import Slide10 from "./components/Slide10";
+import Slide2 from "./components/Slide2";
+import Slide3 from "./components/Slide3";
+import Slide6 from "./components/Slide6";
+import Slide8 from "./components/Slide8";
+import Slide9 from "./components/Slide9";
 import Slide11 from "./components/Slide11";
+import Slide12 from "./components/Slide12";
 
 import "./App.css";
 
@@ -11,14 +17,20 @@ function App() {
   return (
     <div className=".App">
       <Slide1></Slide1>
-
+      <Slide2></Slide2>
+      <Slide3></Slide3>
       <Slide4></Slide4>
       <Slide5></Slide5>
+      <Slide6></Slide6>
 
       <Slide7></Slide7>
+      <Slide8></Slide8>
+
+      <Slide9></Slide9>
 
       <Slide10></Slide10>
       <Slide11></Slide11>
+      <Slide12></Slide12>
     </div>
   );
 }
